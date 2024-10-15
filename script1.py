e4c416f0383023ec8b3cda436b1347ed6839e92b
jolech <jeremiasz.olech@wp.pl>
2024-10-15 13:16:30 +0200

    Calculated days from Ulam's birthday in script1.py

--- script1.py
+++ script1.py
@@ -5,4 +5,4 @@ print('Hello word')
 
 ulams_birthdate = date(1909, 4, 14)
 
-print("Number of days since Ulam's birthady: " , date.today() - ulams_birthdate)
+print("Number of days since Ulam's birthady: " , (date.today() - ulams_birthdate).days)
