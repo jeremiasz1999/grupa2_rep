1cf39ac62060208bbf6426d15ba7427d8c553714
jolech <jeremiasz.olech@wp.pl>
2024-10-21 08:49:05 +0200

    Calculated days from Ulam's birthday in script1.py

--- script1.py
+++ script1.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 
 print('Hello word')
@@ -6,3 +6,13 @@ print('Hello word')
 ulams_birthdate = date(1909, 4, 14)
 
 print("Number of days since Ulam's birthady: " , (date.today() - ulams_birthdate).days)
+
+year = int(input("Enter your birth year: "))
+month = int(input("Enter your birth month: "))
+day = int(input("Enter your birth day: "))
+
+bithdate = date(year, month, day)
+your_age = date.today().day
+
+ulam_days = (date.today() - ulams_birthdate)
+print(f'Ulam was {ulam_days - your_age.day} days old when you were born')
