217081a72a38e5f061edac13a554c1fea5b58fff
jolech <jeremiasz.olech@wp.pl>
2024-11-19 13:55:54 +0100

    data_type improved i opop

--- notebooks/opop_intro.py
+++ notebooks/opop_intro.py
@@ -20,9 +20,15 @@ import matplotlib.pyplot as plt
 
 # %%
 class Population:
+    """ A population of cratures.
 
-    
+    Attributes:
+     specimens (set): A set of Creature instances.
+     history (list): A list of population sizes.
+    """
+
     def __init__(self, n=100):
+        """initalize population w creatures"""
         self.specimens = {Creature() for _ in range(n)}
         self.history = []
 
@@ -37,7 +43,8 @@ class Population:
         self._specimens = value
         self.n = len(value)
 
-    def natural_selection(self):   
+    def natural_selection(self):
+        """Model of natural selection process: kill and reproduce"""
         # Próbujemy zabić wszystkie stwory (dla każdego odpalamy .kill)
         #for specimen in self.speciemens:
             #specimen.kill()
@@ -63,29 +70,39 @@ class Population:
         
         plt.hist([getattr(specimen, parameter) for specimen in self.specimens])
 
+
 class Probability:
+    """Descrpitor for probability attributes"""
+
+    def __set_name__(self, owner, name): # name = 'p_death' albo 'p_reproduce'
+        # self.public_name = name 
+        self.private_name = '_' + name
 
     def __get__(self, obj, objtype=None): # będizemy odczytywać wartość zapisaną gdzie indziej
         # Wartość będize zapisana w _p_death
-        return obj._p_death
+        return getattr(obj,self.private_name) # Zamiast _p_death tu powinna być dowolna nazwa (np. p_reproduce). 
+                                              # Tu używamy self.private_name, który jest str
 
     def __set__(self, obj, value): # tutaj chcemy pinować właściwych wartości ( 0 =<value =< 1)
-        if value < 0:
-            obj._p_death = 0
-        elif value > 1:
-            obj._p_death = 1
-        else:
-            obj._p_death = value
+        setattr(obj, self.private_name, min(1, max(0, value))) # Tu też
+        
+        #if value < 0:
+        #    obj._p_death = 0
+        #elif value > 1:
+        #    obj._p_death = 1
+        #else:
+        #    obj._p_death = value
+
 
 class Creature:
+"""A creature with probability of death and reproduction."""
 
-    sigma = 0.02
+    sigma = 0.02 # Stadnard diviation random component od mutation
     p_death = Probability()
+    p_reproduce = Probability()
     
     alive = True  # Atrybut
-    p_death = 0.2
-    p_reproduce = 0.2
-
+    
     def __init__(self, p_death=0.2, p_reproduce=0.2):
         self.p_death = p_death
         self.p_reproduce = p_reproduce
@@ -94,6 +111,7 @@ class Creature:
     def kill(self):  # Metoda
         if random.random() <= self.p_death:
             self.alive = False
+            
     def reproduce(self):  
         if random.random() <= self.p_reproduce and self.alive: 
             return Creature(p_death = self.p_death + random.gauss(mu=0, sigma=Creature.sigma),
@@ -164,5 +182,15 @@ population.plot_histogram('p_death')
 
 # %%
 population.plot_histogram('p_reproduce')
+
+# %%
+my_creature = Creature()
+
+# %%
+my_creature.p_death
+
+# %%
+my_creature.p_death = 500
+my_creature.p_death
 
 # %%
