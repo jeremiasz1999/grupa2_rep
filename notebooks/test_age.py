c2723edb75f8a84753761f6cc57e4f74a1181468
jolech <jeremiasz.olech@wp.pl>
2024-12-10 13:53:51 +0100

    yes

--- notebooks/test_age.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from age import categorize_by_age
-
-
-class TestCategorizeByAge(unittest.TestCase):
-
-    def test_child(self):
-        self.assertEquals(categorize_by_age(5) == 'Child')
---
+++ test_age1.py
@@ -0,0 +1,39 @@
+import unittest
+from age1 import categorize_by_age
+
+
+class TestCategorizeByAge(unittest.TestCase):
+
+    def test_child(self):
+        self.assertEqual(categorize_by_age(5), 'Child')
+
+    def test_teenager(self):
+        self.assertEqual(categorize_by_age(15),'Teenager')
+
+    def test_adult(self):
+        self.assertEqual(categorize_by_age(30), 'Adult')
+
+    def test_senior(self):
+        self.assertEqual(categorize_by_age(70), 'Senior')
+
+    def test_negative_age(self):
+        self.assertEqual(categorize_by_age(-5), 'Invalid age: -5.')
+
+    # @unittest.skip('Not implemented yet.')
+    def test_invalid_value(self):
+
+        with self.assertRaises(ValueError):
+            categorize_by_age('Hello')
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+
+# Oprccz unittest.skip mamy jeszcze:
+# - unittest,skipIf(condition, reason)
+#   np. condition = sys.version_info < (3,3)
+#
+# - unittest.skipUnless(condition, reason)
+
+
+# Tutaj sobie wypiszemy inne funkcje z grupy "assert"
