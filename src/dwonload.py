e373414ff50c92a4a57b96df93fcd937774bd97b
jolech <jeremiasz.olech@wp.pl>
2025-01-07 13:56:02 +0100

    yes

---
+++ src/dwonload.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+import pandas as pd
+
+COLUMN_NAMES = ['sepal_length',
+ 'sepal_width',
+ 'petal_length',
+ 'petal_width']
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(iris['data'])
+df.columns = COLUMN_NAMES
+print(df)
+#print(iris)
+#print(type(iris))
+#print(iris['target_names'])
