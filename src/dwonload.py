fa0e4c506a731551fe62ee73c4fed0fb136864f3
jolech <jeremiasz.olech@wp.pl>
2025-01-13 09:39:36 +0100

    added data to gitignore, added config.py and download, utils

--- src/dwonload.py
+++ src/dwonload.py
@@ -1,16 +1,27 @@
 from sklearn import datasets
 import pandas as pd
+from utils import get_repo_path
+import config as cfg
 
-COLUMN_NAMES = ['sepal_length',
- 'sepal_width',
- 'petal_length',
- 'petal_width']
+def main():
+    iris = datasets.load_iris()
 
-iris = datasets.load_iris()
+    # Create dataframe
+    df = pd.DataFrame(iris['data'])
+    df.columns = cfg.COLUMN_NAMES
 
-df = pd.DataFrame(iris['data'])
-df.columns = COLUMN_NAMES
-print(df)
-#print(iris)
-#print(type(iris))
-#print(iris['target_names'])
+    # Chcę dorzucić nową kolumnę, która będzie zwierać gatunki kalp "nazwy" (setosa, virginica, itd.)
+
+    df[cfg.TARGET] = iris['target']
+    target_names = {index: name for index, name in enumerate(iris['target_names'])}
+    df[cfg.TARGET] = df[cfg.TARGET].replace(target_names)
+
+    df.to_csv(get_repo_path() / cfg.DATA_PATH / cfg.INPUT_FILE, index=False)
+
+    #print(df)
+    #print(iris)
+    #print(type(iris))
+    #print(iris['target_names'])
+
+if __name__ == '__main__':
+    main()
